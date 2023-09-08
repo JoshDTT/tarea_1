e5ff9f9aa3cef96346caab0397ecf99222a12bf1
Joshua Torres <joshtorrestapia@gmail.com>
2023-09-07 19:12:10 -0600

    ya estuve estudiando un poco y logré que me corriera el programa, lastimosamente fue fuera de tiempo, una disculpa profesor ojalá aún lo pueda tomar en cuenta

--- tarea_1RNA.py
+++ tarea_1RNA.py
@@ -1,14 +1,22 @@
 
-import mnist.pkl
+import mnist_loader
 import network
 import pickle
 
 
-training_data, validation_data , test_data = mnist.pkl.load_data_wrapper() #introducimos las imagenes de mnist como nuestros datos de prueba
+training_data, validation_data , test_data = mnist_loader.load_data_wrapper() #introducimos las imagenes de mnist como nuestros datos de prueba
 
 training_data = list(training_data) #hacemos que nuestros datos de entrenamiento tengan forma de lista 
 test_data = list(test_data) #lo mismo que el paso anterior pero con los datos de prueba
-net=network.Network([256,10,10])
+
+# Creamos un objeto del tipo Network 
+estruc = [784,30,10]
+network =network.Network([784,30,10])
+
+# Llamemos a la funcion SGD para configurar y entrenar la red
+network.SGD(training_data, 30, 30, 3.0, test_data=test_data)
+
+
 
 #archivo = open("red_prueba1.pkl",'wb')
 #pickle.dump(net,archivo)
@@ -16,10 +24,10 @@ net=network.Network([256,10,10])
 #exit()
 
 #leer el archivo
-archivo_lectura = open("red_prueba1.pkl",'rb')
-net = pickle.load( archivo_lectura)
-archivo_lectura.close()
-archivo = open("red_prueba1.pkl",'wb')
-pickle.dump(net,archivo)
-archivo.close()
-exit()
+#rchivo_lectura = open("red_prueba1.pkl",'rb')
+#et = pickle.load( archivo_lectura)
+#rchivo_lectura.close()
+#rchivo = open("red_prueba1.pkl",'wb')
+#ickle.dump(net,archivo)
+#rchivo.close()
+#xit()e
